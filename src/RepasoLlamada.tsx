50b395a7b6e2ede5c3b70979c76977c6c29df675
Luciana <luciana.holzmann@gmail.com>
2024-04-17 00:34:32 +0200

    Repaso

--- src/RepasoLlamada.tsx
+++ src/RepasoLlamada.tsx
@@ -1,47 +1,59 @@
-import { useState } from "react";
+//HOOK
+import {useState} from 'react'
 
-const useProduct = () =>{
-    const [isLoading, setIsLoading] useState(false);
+const useRepaso = () => {
+    const [isLoading, setIsLoading] = useState(false);
 
-    const getProducts = async() =>{
-        setIsLoading(true)
+   
 
+        const getRepaso = async() =>{
+            setIsLoading(true);
         try{
-            const respuesta = await fetch('URL');
+            const respuesta = await fetch(URL);
             const data = respuesta.json();
 
-            return data;
-        } catch(error){
-        throw error;
+            return data
+        }catch(error) {
+            throw error;
         } finally{
-            setLoading(false)
+            setIsLoading(false)
         }
     }
 
-    return{isLoading, getProducts}
+  return {
+    isLoading, getRepaso
+  }
 }
 
-import useProduct from ...
+export default useRepaso
+
+
+
+//LLAMADA
+import {useState} from 'react'
 
 const Home = () => {
-    const {isLoading, getProducts} = useProduct();
-    const [products, setProducts] = useState('');
+    const {isLoading, getRepaso} = useRepaso();
+    const [productos, setProductos] = useState('');
 
-    const callApi = async(e)=>{
+    const callAPI = async(e) =>{
         e.preventDefault();
 
         try{
-            const respuesta = await getProducts();
-            setProducts(respuesta)
-        } catch (error){
+            const respuesta = await getRepaso(productos);
+            setProductos(respuesta)
+        } catch(error){
             throw error;
         }
+    };
 
 
 
-
-    }
-
-
-
+  return (
+    <div>
+      
+    </div>
+  )
 }
+
+export default Home
